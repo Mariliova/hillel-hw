b170207031a562dc489f9f0caac5c0129bfe7b2d
Mariia Kovalova <mashuha1698@gmail.com>
2022-10-07 15:59:01 +0300

    init

---
+++ js/hw4.js
@@ -0,0 +1,18 @@
+const userAnswer = prompt("Would you like 'add', 'sub, 'mult' or 'div'?", "add");
+if (userAnswer == 'add' || userAnswer == 'sub' || userAnswer == 'mult' || userAnswer == 'div') {
+    const userNum1 = prompt("Enter a number", "33");
+    const userNum2 = prompt("Enter a number", "11");
+if (userAnswer == 'add') {
+    alert(`${userNum1} + ${userNum2} = ${Number(userNum1) + Number(userNum2)}`);
+} else if (userAnswer == 'sub') {
+    alert(`${userNum1} - ${userNum2} = ${Number(userNum1) - Number(userNum2)}`);
+} else if (userAnswer == 'mult') {
+    alert(`${userNum1} * ${userNum2} = ${Number(userNum1) * Number(userNum2)}`);
+} else {
+    alert(`${userNum1} / ${userNum2} = ${Number(userNum1) / Number(userNum2)}`);
+}
+} else {
+    alert("You entered wrong command. Please renovate the page and try again. ")
+}
+
+
