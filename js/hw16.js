ccf1f593c9c59e4f18ba41c970ede2e019a60eac
Mariia Kovalova <mashuha1698@gmail.com>
2022-10-23 23:11:25 +0300

    hw16

--- js/hw16.js
+++ js/hw16.js
@@ -5,10 +5,16 @@ function pow(num, degree) {
   if (num === 0) return 0;
   if (degree === 0) return 1;
   if (degree === 1) return num;
-  if (degree > 1 && num > 0) {
+  if (degree > 1) {
     return num * pow(num, degree - 1);
+  }
+  if (degree < 0) {
+    return 1.0 / pow(num, -degree);
   }
 }
 
-const number = pow(2, 4);
-console.log(number);
+const number1 = pow(-2, 3);
+console.log(number1);
+
+const number2 = pow(2, -3);
+console.log(number2);
