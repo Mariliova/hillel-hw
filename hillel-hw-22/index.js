fd10fac77ea4889422bb059a6e7e6192c82779a4
Mariia Kovalova <mashuha1698@gmail.com>
2022-11-08 22:24:52 +0200

    hw22

--- hillel-hw-22/index.js
+++ hillel-hw-22/index.js
@@ -1,5 +1,3 @@
-const numbers = Array.from({ length: 100 }, (_, i) => i + 1);
-
 const table = document.createElement("table");
 const container = document.querySelector(".container");
 container.append(table);
@@ -7,10 +5,9 @@ container.append(table);
 for (let i = 1; i <= 10; i += 1) {
     const tr = document.createElement("tr");
     table.append(tr);
-    for (let i = 1; i <= 10; i += 1){
+    for (let j = 1; j <= 10; j += 1){
         const td = document.createElement("td");
-        const text = numbers.splice(0, 1);
         tr.append(td);
-        td.append(text);
+        td.append(`${j*i}`);
     }
 }
