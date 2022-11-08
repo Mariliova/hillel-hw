c2c2942b73f0f02b698f9c5e0b333bcfc0becc4a
Mariia Kovalova <106843479+Mariliova@users.noreply.github.com>
2022-11-08 10:27:11 +0200

    Update hillel-hw-23/index.js
    Co-authored-by: Vasyl Boyko <43011715+vb-oiko@users.noreply.github.com>

--- hillel-hw-23/index.js
+++ hillel-hw-23/index.js
@@ -1,5 +1,5 @@
 const arr = ["./img/dog1.jpeg", "./img/dog2.jpeg", "./img/dog3.jpeg", "./img/dog4.jpeg", "./img/dog5.jpeg"];
 const img = document.querySelector("img");
 
-const randomKey = arr[Math.floor(Math.random() * arr.length)];
+const randomImgSrc = arr[Math.floor(Math.random() * arr.length)];
 img.setAttribute("src", randomKey);
