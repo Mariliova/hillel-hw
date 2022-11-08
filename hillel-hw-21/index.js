80d459d514711e1b43a4f49e47ed91edb78cdd21
Mariia Kovalova <mashuha1698@gmail.com>
2022-11-08 22:14:58 +0200

    hw21

--- hillel-hw-21/index.js
+++ hillel-hw-21/index.js
@@ -7,10 +7,6 @@ const handleInput1 = (input.onblur = () => {
   ghost.remove();
 });
 
-input.addEventListener("blur", handleInput1);
-
 const handleInput2 = (input.onfocus = () => {
   container.appendChild(ghost);
 });
-
-input.addEventListener("focus", handleInput2);
