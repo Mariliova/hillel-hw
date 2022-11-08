b99df698a7c7fb3a03fc45d8ad9d025230a2c466
Mariia Kovalova <mashuha1698@gmail.com>
2022-11-08 23:52:54 +0200

    hw24

--- hillel-hw-24/index.js
+++ hillel-hw-24/index.js
@@ -1,17 +1,42 @@
-{/* <div class="container">
-            <button id="prev">Prev</button>
-            <img src="./img/dog1.jpeg" alt="dog1">
-            <button id="next">Next</button>
-</div> */}
-
-const arr = [
+const imgSrcs = [
   "./img/dog1.jpeg",
   "./img/dog2.jpeg",
   "./img/dog3.jpeg",
   "./img/dog4.jpeg",
   "./img/dog5.jpeg",
+  "./img/dog6.jpeg",
 ];
-const img = document.querySelector("img");
 
-const randomKey = arr[Math.floor(Math.random() * arr.length)];
-img.setAttribute("src", randomKey);
+const img = document.querySelector("img");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+prev.hidden = true;
+
+const nextImg = (next.onclick = () => {
+  const currentImgAttr = img.getAttribute("src");
+  const currentImgScrIndex = imgSrcs.indexOf(currentImgAttr);
+  img.setAttribute("src", `${imgSrcs[currentImgScrIndex + 1]}`);
+
+  if (imgSrcs.length - currentImgScrIndex >= 2) {
+    prev.hidden = false;
+  }
+
+  if (imgSrcs.length - 2 == currentImgScrIndex) {
+    next.hidden = true;
+  }
+});
+
+const prevImg = (prev.onclick = () => {
+  const currentImgAttr = img.getAttribute("src");
+  const currentImgScrIndex = imgSrcs.indexOf(currentImgAttr);
+  img.setAttribute("src", `${imgSrcs[currentImgScrIndex - 1]}`);
+
+  if (currentImgScrIndex == 1) {
+    prev.hidden = true;
+  }
+
+  if (imgSrcs.length - 1 == currentImgScrIndex) {
+    next.hidden = false;
+  }
+});
