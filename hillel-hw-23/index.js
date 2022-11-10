2a03581514872c4532d93310e319efeef92eade2
Mariia Kovalova <mashuha1698@gmail.com>
2022-11-10 10:28:55 +0200

    hw23

--- hillel-hw-23/index.js
+++ hillel-hw-23/index.js
@@ -1,4 +1,10 @@
-const arr = ["./img/dog1.jpeg", "./img/dog2.jpeg", "./img/dog3.jpeg", "./img/dog4.jpeg", "./img/dog5.jpeg"];
+const arr = [
+  "./img/dog1.jpeg",
+  "./img/dog2.jpeg",
+  "./img/dog3.jpeg",
+  "./img/dog4.jpeg",
+  "./img/dog5.jpeg",
+];
 const img = document.querySelector("img");
 
 const randomImgSrc = arr[Math.floor(Math.random() * arr.length)];
