f842524e1808fd6d0300687940ca97f3f7bd079e
Mariia Kovalova <mashuha1698@gmail.com>
2022-11-19 00:52:18 +0200

    hw28

--- hillel-hw-28/index.js
+++ hillel-hw-28/index.js
@@ -1,1 +1,35 @@
 const listOfLists = [1, 2, [1, 2, 3], 3, 4, [1, 2, [1, 2, 3]], 5];
+const container = document.querySelector(".container");
+const base = [];
+
+function generateList(array) {
+  const ul = document.createElement("ul");
+
+  for (let i = 0; i <= array.length; i += 1) {
+    if (i === array.length) {
+      base.length = 0;
+      return ul;
+    }
+
+    if (!Array.isArray(array[i])) {
+      if (base.length === 0) {
+        ul.insertAdjacentHTML("beforeend", `<li>${array[i]}</li>`);
+      } else {
+        ul.insertAdjacentHTML(
+          "beforeend",
+          `<li>${base.join(".")}.${array[i]}</li>`
+        );
+      }
+
+      if (Array.isArray(array[i + 1])) {
+        base.push(array[i]);
+      }
+    }
+
+    if (Array.isArray(array[i])) {
+      ul.insertAdjacentHTML("beforeend", generateList(array[i]).outerHTML);
+    }
+  }
+}
+
+container.appendChild(generateList(listOfLists));
