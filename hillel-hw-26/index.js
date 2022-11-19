42f3f603d728a31b4347e7818c24b4a4d7c9b056
Mariia Kovalova <mashuha1698@gmail.com>
2022-11-20 00:11:21 +0200

    hw26

--- hillel-hw-26/index.js
+++ hillel-hw-26/index.js
@@ -9,14 +9,10 @@ const categories = [
   },
   {
     id: 3,
-    category: "Toys",
-  },
-  {
-    id: 4,
     category: "Grocery",
   },
   {
-    id: 5,
+    id: 4,
     category: "Garden",
   },
 ];
@@ -24,29 +20,29 @@ const categories = [
 const products = [
   {
     id: 1,
-    category_id: 2,
+    category_id: 3,
     name: "Eggplant - Asian",
     description:
       "Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
   },
   {
     id: 2,
-    category_id: 5,
+    category_id: 4,
     name: "Jagermeister",
     description:
       "Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
   },
   {
     id: 3,
-    category_id: 2,
+    category_id: 3,
     name: "Wine - Lou Black Shiraz",
     description:
       "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.",
   },
   {
     id: 4,
-    category_id: 3,
-    name: "Beef - Ox Tongue",
+    category_id: 1,
+    name: "Toy car - Hot Wheels",
     description:
       "Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
   },
@@ -60,13 +56,13 @@ const products = [
   {
     id: 6,
     category_id: 2,
-    name: "Appetizer - Shrimp Puff",
+    name: "Tracksuit - Adidas",
     description:
       "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus.",
   },
   {
     id: 7,
-    category_id: 4,
+    category_id: 3,
     name: "Appetizer - Shrimp Puff",
     description:
       "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
@@ -93,7 +89,7 @@ const products = [
   },
   {
     id: 11,
-    category_id: 1,
+    category_id: 3,
     name: "Beer - Guiness",
     description:
       "In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
@@ -108,22 +104,60 @@ const products = [
   {
     id: 13,
     category_id: 2,
-    name: "Oil - Truffle, White",
+    name: "snikers - Nike",
     description:
       "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
   },
   {
     id: 14,
-    category_id: 5,
+    category_id: 4,
     name: "Wine - Puligny Montrachet A.",
     description:
       "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat.",
   },
   {
     id: 15,
-    category_id: 5,
+    category_id: 4,
     name: "Tomatoes Tear Drop",
     description:
       "Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.",
   },
 ];
+
+const categoriesUl = document.querySelector("#categories");
+const productsUl = document.querySelector("#products");
+const message = document.querySelector("#message");
+const description = document.querySelector("#description");
+const button = document.querySelector("#buy-button");
+
+categoriesUl.addEventListener("click", (event) => {
+  if (event.target.nodeName === "LI") {
+    productsUl.innerHTML = "";
+    for (const product of products) {
+      if (product.category_id === Number(event.target.dataset.categoryId)) {
+        productsUl.insertAdjacentHTML(
+          "beforeend",
+          `<li data-product-id=${product.id}>${product.name}</li>`
+        );
+      }
+    }
+  }
+});
+
+productsUl.addEventListener("click", (event) => {
+  if (event.target.nodeName === "LI") {
+    description.innerText = "";
+    message.innerText = "";
+    for (const product of products) {
+      description.innerText = product.description;
+      message.innerText = "The product has been added to the basket";
+    }
+  }
+});
+
+button.addEventListener("click", () => {
+  if (message.innerText === "The product has been added to the basket") {
+    alert("The product was bought! Thank you!");
+  }
+  window.location.reload(true);
+});
